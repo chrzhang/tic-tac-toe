95acfd421f10cbd8a5d071f3021663421c3d6755
Christopher Zhang <christopherzhang3@gmail.com>
2020-01-05 16:53:26 -0500

    Updates

---
+++ precache-manifest.3096b62dfa3b35d1f85140102f04206e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070fbf0d418f581db225b0fd73d84906",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "22c33f3482d53ee731af",
+    "url": "/tic-tac-toe/static/css/main.677b89b8.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "22c33f3482d53ee731af",
+    "url": "/tic-tac-toe/static/js/main.717e9a0b.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
